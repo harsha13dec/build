86391bc4fe12c80d331f6f32b01742f0a7813c4e
harsha13dec <105868260+harsha13dec@users.noreply.github.com>
2022-07-27 11:33:54 +0530

    second line added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "welcome to the new world. World!" );
+        System.out.println( "I am in a new image right now and I am liking it!" );
     }
 }
